6f319019d71cf06bd4aaeb0189b859b030f29c19
Kevin <b0952193319@gmail.com>
2025-01-17 13:20:03 +0800

    new project

---
+++ src/test/java/org/example/OrderCalculatorTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class OrderCalculatorTest {
+
+
+
+    @Test
+    void back_to_base_game() {
+
+    }
+
+
+}
