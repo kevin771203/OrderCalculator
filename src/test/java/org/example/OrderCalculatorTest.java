09757295ca3707dcaf43515b878a1922fe5d359b
Kevin <b0952193319@gmail.com>
2025-04-17 15:12:21 +0800

    Rename "banana" to "bananaa" in test cases

--- src/test/java/org/example/OrderCalculatorTest.java
+++ src/test/java/org/example/OrderCalculatorTest.java
@@ -12,13 +12,13 @@ class OrderCalculatorTest {
         OrderCalculator orderCalculator = new OrderCalculator();
 
         List<Item> items = orderCalculator.addItems(new Item("apple", 30, 2));
-        orderCalculator.addItems(new Item("banana", 50, 3));
+        orderCalculator.addItems(new Item("bananaa", 50, 3));
         Item item = items.get(0);
         Assertions.assertThat(item.getName()).isEqualTo("apple");
         Assertions.assertThat(item.getQuantity()).isEqualTo(2);
         Assertions.assertThat(item.getPrice()).isEqualTo(30);
         item = items.get(1);
-        Assertions.assertThat(item.getName()).isEqualTo("banana");
+        Assertions.assertThat(item.getName()).isEqualTo("bananaa");
         Assertions.assertThat(item.getQuantity()).isEqualTo(3);
         Assertions.assertThat(item.getPrice()).isEqualTo(50);
     }
@@ -26,7 +26,7 @@ class OrderCalculatorTest {
     @Test
     void total_price() {
         OrderCalculator orderCalculator = new OrderCalculator();
-        orderCalculator.addItems(new Item("banana", 50, 3));
+        orderCalculator.addItems(new Item("bananaa", 50, 3));
         orderCalculator.addItems(new Item("apple", 30, 2));
         Assertions.assertThat(orderCalculator.totalPrice()).isEqualTo(210);
     }
